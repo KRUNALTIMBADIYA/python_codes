e61d9c1c8945c181fb1e0e93861d4a631f9ed848
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2023-06-17 19:49:04 +0530

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,6 +1,6 @@
-def factors(n):             # traditional function that computes factors
-  results = []              # store factors in a new list
+def factors(n):             
+  results = []             
   for k in range(1,n+1):
-    if n % k == 0:          # divides evenly, thus k is a factor
-      results.append(k)     # add k to the list of factors
-  return results            # return the entire lis
+    if n % k == 0:          
+      results.append(k)    
+  return results           
